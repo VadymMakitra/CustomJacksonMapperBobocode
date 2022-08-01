bcb99648c37a155e189f95434a29d150fe2acd16
VadymMakitra <vmaki@softserveinc.com>
2022-08-01 15:34:03 +0300

    Initial commit

---
+++ src/CustomJacksonMapper.java
@@ -0,0 +1,63 @@
+// Create the methods that convert a value from JSON to Java Object
+// if the new fields are added method should accept them and set values to this field.
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class CustomJacksonMapper {
+    public static void main(String[] args) throws InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
+        var json = "{\n" +
+            "  \"firstName\": \"Vadym\",\n" +
+            "  \"lastName\": \"Makitra\",\n" +
+            "  \"phoneNumber\": \"+38099999999\",\n" +
+            "  \"email\": \"hmarax3@gmail.com\"\n" +
+            "}";
+
+        var user = jsonToObject(json, User.class);
+        System.out.println(user);
+    }
+
+    private static <T> T jsonToObject(String json, Class<T> clazz) throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+        Constructor<T> obj = clazz.getConstructor();
+        T convertedObj = obj.newInstance();
+        Arrays.stream(clazz.getDeclaredFields()).forEach((field -> {
+            Pattern pattern = Pattern.compile("\\s?\"" + field.getName() +"\":\\s?\".\\w+@?\\w+.?\\w+");
+            Matcher pet = pattern.matcher(json);
+            String serched = "";
+            if (pet.find()) {
+                serched = pet.group(0);
+                serched = serched.split("\\s?\"" + field.getName() + "\"\\s?:\\s?\"")[1];
+            }
+            try {
+                field.setAccessible(true);
+                field.set(convertedObj, serched);
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
+        }));
+        return convertedObj;
+    }
+
+    static class User {
+        private String firstName;
+        private String lastName;
+        private String phoneNumber;
+        private String email;
+
+        public User() {
+        }
+
+        @Override
+        public String toString() {
+            return "User{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+        }
+    }
+}
